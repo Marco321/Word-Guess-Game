f48ce3480b839025d4d1eb73fd7abc5e501216e2
Marco Saglietto <marcosaglietto@dhcp-v119-098.mobile.uci.edu>
2018-05-11 09:44:45 -0700

    pushing changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,72 @@
-
-var word = ["Steelers","Juventus","Lakers"];
+// Global Variables
+var word = ["steelers","juventus","lakers"];
 
 var random = Math.floor(Math.random() * 3);
 
 var chosenWord = word[random];
 
+var answerDashes = []
+
+var wrongLetters = []
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 5;
 
+var comma = ",";
+var space =" ";
+
+var correctLetters = []
+
+for (i = 0; i < chosenWord.length; i++){
+  console.log(chosenWord[i]);
+};
+
+for (i = 0; i < chosenWord.length; i++){
+    answerDashes[i] = "_";
+};
+
+var dashesNoComma = answerDashes.join(" ");
+
 console.log(random);
 console.log(chosenWord);
-    
-// function blank() {
-//     for ( )
-// };
+console.log(chosenWord.length);
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
     
+    wrongLetters.push(userGuess);
+    var spaceWrongLetters = wrongLetters.join(space);
 
+    console.log(userGuess);
 
-    // if (userGuess == chosenWord.charAt[i]){
+    for (i=0; i < chosenWord.length; i++) {
+        if (userGuess == chosenWord[i]) {
+            console.log("correct");
+            console.log(i);
+            break;
+            answerDashes[i] = userGuess;
 
-    // };
+            // userGuess == correctLetters;
+            // console.log(correctLetters);
+            // dashesNoComma[i] = userGuess;
+            // dashesNoComma.replace([i],"sad" )
+            // dashesNoComma[i] = userGuess;
+            dashesNoComma.replace([i],userGuess);
+            wins++;
+        }; 
+    };
 
-    var html =
-    "<p>             </p>" +
+    var html = 
+    "<p>The word is: " + dashesNoComma + "</p>" +
+    "<p> User Guesses: " + spaceWrongLetters + "</p>" +
     "<p>You chose: " + userGuess + "</p>" +
     "<p>wins: " + wins + "</p>" +
-    "<p>losses: " + losses + "</p>" +
-    "<p>guessesLeft: " + guessesLeft + "</p>";
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>";
 
 
   document.querySelector("#game").innerHTML = html;
+
 
 };
